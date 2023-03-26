e1314baa4fe5b97f1afbf5a77f9ed5bde0227ce5
Adriano Lucio <lucio.adriano@gmail.com>
2023-03-25 22:34:28 -0300

    criando home e styles

--- src/main/java/br/com/htex/hotel/controller/ListaController.java
+++ src/main/java/br/com/htex/hotel/controller/ListaController.java
@@ -6,28 +6,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-//@RestController("/")
-@RequestMapping("/")
 @Controller
+@RequestMapping("/hotel")
 public class ListaController {
     @GetMapping("/listaUsuarios")
     public String listaUsuarios(Model model) {
-        Usuario u1 = new Usuario(1,"Silvio", "andeson");
-        Usuario u2 = new Usuario(1,"Silvio", "andeson");
-        Usuario u3 = new Usuario(1,"Silvio", "andeson");
-        Usuario u4 = new Usuario(1,"Silvio", "andeson");
-
-        model.addAttribute("usuarios", Arrays.asList(u1, u2, u3, u4));
-        System.out.println(model.toString());
-
+        model.addAttribute("usuarios",
+                Arrays.asList(
+                        new Usuario(1,"adriano", "1234"),
+                        new Usuario(1,"silvio", "4321"),
+                        new Usuario(1,"andeson", "3224"))
+        );
         return "listaUsuarios";
     }
     @GetMapping("/listaClientes")
     public String listaClientes(Model model) {
-        model.addAttribute(
-                "clientes",
+        model.addAttribute("clientes",
             Arrays.asList(
                 new Cliente(1, "Adriano","12345678901", new Usuario("adriano","1234")),
                 new Cliente(2, "Silvo","10987654321", new Usuario("silvio","4321")),
